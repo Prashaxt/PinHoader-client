8014d7ea31d6d82d949184cebbdabcc9de15057f
Prashaxt <147325777+Prashaxt@users.noreply.github.com>
2025-07-23 16:19:44 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Layout from "./Layout.jsx"
 import './App.css'
 import Home from "./components/Home.jsx"
-import Features from "./components/Features.jsx"
 
 function App() {
   
 
   return (
     <>
-<BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
       </Routes>
-    </BrowserRouter>
     </>
   )
 }
